3adef812ad0a5a105371a3349190b3daa7f2b6c8
Peijie Hu <peijie@armorway.com>
2017-04-27 16:28:53 -0700

    rename handler; deploy to pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 setup(
-  name = 'kafka-logger',
-  packages = ['kafka-logger'],
-  version = '0.1.0',
-  description = 'A simple Kafka logger in Python.',
-  author = 'Peijie Hu',
-  url = 'https://github.com/peijiehu/kafka-logger',
-  download_url = 'https://github.com/peijiehu/kafka-logger/archive/0.1.0.tar.gz',
-  keywords = ['python', 'kafka', 'logstash', 'logging'],
-  classifiers = [],
+    name='kafka-logger',
+    packages=find_packages(exclude=['build', 'docs', 'templates']),
+    version='0.2.2',
+    description='A simple Kafka logger in Python.',
+    author='Peijie Hu',
+    author_email='peijie@peijiehu.com',
+    license='MIT',
+    url='https://github.com/peijiehu/kafka-logger',
+    keywords=['python', 'kafka', 'logstash', 'logging'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
 )
