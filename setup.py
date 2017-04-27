d73b90a30fed1b21698298a381afcb7aa29026bf
Peijie Hu <peijie@armorway.com>
2017-04-27 13:39:25 -0700

    setup for pypi

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'kafka-logger',
+  packages = ['kafka-logger'],
+  version = '0.1.0',
+  description = 'A simple Kafka logger in Python.',
+  author = 'Peijie Hu',
+  url = 'https://github.com/peijiehu/kafka-logger',
+  download_url = 'https://github.com/peijiehu/kafka-logger/archive/0.1.0.tar.gz',
+  keywords = ['python', 'kafka', 'logstash', 'logging'],
+  classifiers = [],
+)
